607f25e80231dba4afcb075d60f4f3a01fd6b943
SamuraJey <cergeypro703@gmail.com>
2024-10-16 14:56:03 +0500

    Tasks 2-3 done. 4 is started

--- 03threads/main.c
+++ 03threads/main.c
@@ -1,53 +1,73 @@
 // Подключение библиотек
-#include "thread.h"
-#include "xtimer.h"
-#include "timex.h"
 #include "periph/gpio.h"
+#include "thread.h"
+#include "timex.h"
+#include "xtimer.h"
 
 // Выделение памяти под стек первого треда
 // Размер выделяемого стека задан макросом THREAD_STACKSIZE_DEFAULT
 char stack_one[THREAD_STACKSIZE_DEFAULT];
+char stack_two[THREAD_STACKSIZE_DEFAULT];
 
 // Это первый поток
-void *thread_one(void *arg)
-{
+void* thread_one(void* arg) {
     // Прием аргументов из главного потока
-    (void) arg;
+    (void)arg;
     // ВременнАя метка для отсчета времени сна
     xtimer_ticks32_t last_wakeup_one = xtimer_now();
-    while(1){
+    while (1) {
         // Переключение состояния пина PC8
-    	gpio_toggle(GPIO_PIN(PORT_C,8));
+        gpio_toggle(GPIO_PIN(PORT_C, 8));
         // Поток засыпает на 100000 микросекунд
-    	xtimer_periodic_wakeup(&last_wakeup_one, 100000);
+        xtimer_periodic_wakeup(&last_wakeup_one, 100000);
     }
     return NULL;
 }
 
+// это второй поток!!!
+void* thread_two(void* arg) {
+    // Прием аргументов из главного потока
+    (void)arg;
+    // ВременнАя метка для отсчета времени сна
+    xtimer_ticks32_t last_wakeup_one = xtimer_now();
+    while (1) {
+        // Переключение состояния пина PC8
+        gpio_toggle(GPIO_PIN(PORT_C, 9));
+        // Поток засыпает на 1000000 микросекунд (раз в секунду)
+        xtimer_periodic_wakeup(&last_wakeup_one, 1000000);
+    }
+    return NULL;
+}
 
-int main(void)
-{
+int main(void) {
     // Инициализация пина PC8 на выход
-	gpio_init(GPIO_PIN(PORT_C,8), GPIO_OUT);
+    gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
+    gpio_init(GPIO_PIN(PORT_C, 9), GPIO_OUT);
 
     // Создание потока
     // stack_one - выделенная под стек память
     // sizeof(stack_one) - размер стека
     // THREAD_PRIORITY_MAIN-1 - приоритет потока на 1 больше, чем у потока main.
     // THREAD_CREATE_STACKTEST - флаги, которые создают маркеры использования стека
-    // thread_one - имя потока
+    // thread_one - функция - точка входа в новый поток
     // NULL - передаваемые в поток аргументы
     // "thread_one" - дескриптор
     thread_create(stack_one, sizeof(stack_one),
-                    THREAD_PRIORITY_MAIN-1,
-                    THREAD_CREATE_STACKTEST,
-                    thread_one,
-                    NULL, "thread_one");
+                  THREAD_PRIORITY_MAIN - 1,
+                  THREAD_CREATE_STACKTEST,
+                  thread_one,
+                  NULL, "thread_one");
+
+    thread_create(stack_two, sizeof(stack_two),
+                  THREAD_PRIORITY_MAIN - 2,
+                  THREAD_CREATE_STACKTEST,
+                  thread_two,
+                  NULL, "thread_two");
 
     return 0;
 }
 
 /*
-    Задание 1. Создайте второй поток, который будет моргать другим светодиодом с другой частотой. 
+    Задание 1. Создайте второй поток, который будет моргать другим светодиодом с другой частотой.
                 Обратите внимание, что не рекомендуется создавать потоки с одинаковым приоритетом!
 */
